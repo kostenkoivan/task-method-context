9c72b656544fa2e73a2dc1b7b50982e80287ac12
kostenkoivan <bonanzaboss@gmail.com>
2022-04-15 13:11:07 +0300

    Задание Method Context

--- src/MethodContext.js
+++ src/MethodContext.js
@@ -8,8 +8,15 @@ import { EventEmitter } from './EventEmitter';
  */
 export const obj = {
     count: 0,
-    subscribe() {},
-    unsubscribe() {},
+    counter() {
+        obj.count++;
+    },
+    subscribe() {
+        EventEmitter.on('click', this.counter);
+    },
+    unsubscribe() {
+        EventEmitter.off('click', this.counter);
+    },
 };
 
 /*
@@ -19,8 +26,10 @@ obj1.first(1, 2, 3);
 // Внутренний вызов должен быть равносилен obj1.second(3, 2, 1)
  */
 export const obj1 = {
-    first(...args) {},
-    second() {
+    first(...args) {
+        this.second(...args.reverse());
+    },
+    second(...args) {
         // здесь ничего писать не нужно
     },
 };
